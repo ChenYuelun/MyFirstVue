b44d800cc1b6919557e460a7c4299c7f740e11e9
ChenYuelun <chenyuelun@caiqr.com>
2019-12-23 14:30:03 +0800

    firstCommit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  @import url('./assets/css/responsive.css');
+  @import url('./assets/css/normalize.css');
+</style>
